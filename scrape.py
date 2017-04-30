b4c89e90b25dd0ab3e0e7fcaa3d7ca232ae6575b
Shane Brosnan <sbrosna1@nd.edu>
2017-04-30 14:11:36 -0400

    scrape

--- scrape.py
+++ scrape.py
@@ -8,6 +8,7 @@ import Queue
 
 # Functions
 
+#Usage function, called when -h flag or an incorrect flag is passed as a command line argument
 def usage(status=0):
     print '''Usage: {} [ -l LENGTH -s SYMBOL ]
     -l LENGTH        How many days to use for time average (recommended: 10)
@@ -18,6 +19,8 @@ def usage(status=0):
     )
     sys.exit(status)
 
+#Get Close Prices function, called when mean reversion algorithm is run
+#Downloads appropriate data from API and returns a list containing the closing prices from the past (length) days
 def getClosePrices(function, symbol, apikey, length, printPrices):
     link = 'http://www.alphavantage.co/query?function=' + function + '&symbol=' + symbol + '&apikey=' + apikey
 
@@ -25,7 +28,7 @@ def getClosePrices(function, symbol, apikey, length, printPrices):
 
     data = r.json()
     dates = data['Time Series (Daily)'].keys()
-    dates.sort();
+    dates.sort();#sort dates so data is iterated through in chronological order
     closePrices = []
     for i in range(1, length + 1):
         closePrices.append(float(data['Time Series (Daily)'][dates[-1 * i]].get('4. close', None)))
@@ -33,6 +36,9 @@ def getClosePrices(function, symbol, apikey, length, printPrices):
             print data['Time Series (Daily)'][dates[-1 * i]].get('4. close', None)
     return closePrices
 
+#Get Mean function, called when mean reversion algorithm is run
+#Calls getClosePrices algorithm and calculates mean value of list that function returns
+#returns a list containing both the mean of the list of prices and the list of prices itself
 def getMean(function, symbol, apikey, length, printPrices):
     closePrices = getClosePrices(function, symbol, apikey, length, printPrices)
 
@@ -46,6 +52,7 @@ def getMean(function, symbol, apikey, length, printPrices):
     data.append(closePrices)
     return data
 
+#MACD function, used when running MACD algorithm
 def MACD(function, symbol, interval, series_type, apikey, length):
     link = 'http://www.alphavantage.co/query?function=' + function + '&symbol=' + symbol + '&interval=' + interval + '&series_type=' + series_type + '&apikey=' + apikey
 
@@ -74,6 +81,7 @@ args = sys.argv[1:]
 singleStock = 0
 SP500MACD = 0
 
+#Parse command line arguments
 while len(args) and args[0].startswith('-') and len(args[0]) > 1:
     arg = args.pop(0)
     if arg == '-s':
@@ -87,9 +95,12 @@ while len(args) and args[0].startswith('-') and len(args[0]) > 1:
        usage(0)
     else:
        usage(1)
-    
+
+#variables used in API call    
 function = 'TIME_SERIES_DAILY'
 apikey = 'ZK41'
+
+#Code for downloading list of indexes currently in S&P 500
 sp500link = "https://raw.githubusercontent.com/datasets/s-and-p-500-companies/master/data/constituents.csv"
 r = requests.get(sp500link)
 fileName = 'indexes.csv'
@@ -104,6 +115,8 @@ with open(fileName, 'r') as file:
     for stock in l:
         indexes.append(stock[0])
 
+#If -m flag is passed as command line argument
+#Run MACD algorithm on each index in S&P 500. If index passes MACD test, also run mean reversion algorithm on index (since so few stocks pass MACD test)
 if SP500MACD:
     for count, index in enumerate(indexes):
         if count is 0:
@@ -126,20 +139,21 @@ if SP500MACD:
         else:
             print
 
-
+#If single stock option is chosen by user with -s flag
+#Run both MACD algorithm and mean reversion algorithm on stock and make suggestion
 elif singleStock:
     data = getMean(function, symbol, apikey, length, 1)
     MACDResult = MACD('MACD', symbol, 'daily', 'close', apikey, length)
     print "Mean Price over Period:", data[0]
     print "Current Price:", data[1][0]
 
-    if data[1][0] < data[0] and MACDResult == 1:
+    if data[1][0] < data[0] and MACDResult == 1:#Passes both algorithms
         print "MACD Result: BUY"
         print "Suggestion: STRONG BUY"
-    elif data[1][0] < data[0]:
+    elif data[1][0] < data[0]:#Passes mean reversion only
         print "MACD Result: Inconclusive"
         print "Suggestion: WEAK BUY"
-    elif data[1][0] > data[0] and MACDResult == -1:
+    elif data[1][0] > data[0] and MACDResult == -1: #Fails both algorithms
         print "MACD Result: SELL"
         print "Suggestion: STRONG SELL"
     else:
@@ -147,8 +161,7 @@ elif singleStock:
         print "Suggestion: WEAK SELL" 
 
 else:
-    #stockRank = [] # Priority Queue for ranking stocks
-    q = Queue.PriorityQueue()
+    q = Queue.PriorityQueue() #Priority Queue for holding top 10 stocks
 
     for count, index in enumerate(indexes):
         if count is 0:
@@ -160,26 +173,12 @@ else:
         print 'Processing', index, '...'
         data = getMean(function, index, apikey, length, 0)
         percentDiff = data[1][0] / data[0]
-        tup = (percentDiff, index)
-        '''if count < 11:
-            #stockRank.append(tup)
-            q.put(tup)
-            #stockRank.sort()
-        else:
-            #myTuple = stockRank[9]
-            myTuple = q[9]
-            percent = myTuple[0]
-            if percentDiff < percent:
-                stockRank.pop()
-                stockRank.append(tup)
-                stockRank.sort()'''
+        tup = (percentDiff, index)     
         q.put(tup)
 
     print
     print 
     print "Top 10"
-    '''for count, stock in enumerate(q):
-        print count + 1, stock[1], stock[0]'''
     for i in range(10):
         stock = q.get()
         print i + 1, stock[1], stock[0]
